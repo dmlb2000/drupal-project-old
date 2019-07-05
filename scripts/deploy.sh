af6eb84286c8669cb2063681eb26202a1aa7f8de
David Brown <dmlb2000@gmail.com>
2019-07-05 09:03:36 -0700

    add some config to drush things into place
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -e
 
 PROFILE=${1:-dmlb2000}
+export PATH="$PWD/vendor/bin:$PATH"
 
 pushd scripts/local-docker
 docker-compose build --pull
 docker-compose pull
 docker-compose up -d
 popd
-echo $PROFILE
+composer install
+drush -y rsync . @docker.local
+drush -y @docker.local si --db-url=mysql://drupal:drupal@drupaldb:3306/drupal --site-name="$PROFILE site" $PROFILE
