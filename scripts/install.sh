4ffa404f6e7662ae2719cd52201ce6395d8b9794
David Brown <dmlb2000@gmail.com>
2019-07-09 14:43:26 -0700

    Add more project stuff
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,5 +6,8 @@ composer install
 composer update
 drush -y rsync . @docker.local
 drush -y @docker.local si --db-url=mysql://drupal:drupal@drupaldb:3306/drupal --site-name="$PROFILE site" $PROFILE
-drush -y @docker.local en devel devel_php devel_generate easy_install php pacifica_uploader pacifica_synch
+drush -y @docker.local en devel devel_php devel_generate easy_install php pacifica_uploader pacifica_synch config_inspector
+drush -y @docker.local cset pacifica_synch.settings pacifica_synch.enabled true
+drush -y @docker.local cset pacifica_synch.settings pacifica_synch.metadata.host metadataserver
+drush -y @docker.local cset pacifica_synch.settings pacifica_synch.amqp.host drupalamqp
 drush @docker.local uli
