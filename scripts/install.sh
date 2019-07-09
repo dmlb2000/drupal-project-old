ea88ea05ed9ed616d350a0943ba7f66e7acc12a9
David Brown <dmlb2000@gmail.com>
2019-07-09 07:45:47 -0700

    Add Pacifica Synch module
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -xe
 
+PROFILE=${PROFILE:-standard}
 export PATH="$PWD/vendor/bin:$PATH"
 composer install
 composer update
 drush -y rsync . @docker.local
 drush -y @docker.local si --db-url=mysql://drupal:drupal@drupaldb:3306/drupal --site-name="$PROFILE site" $PROFILE
-drush -y @docker.local en devel devel_php devel_generate easy_install php pacifica_uploader
+drush -y @docker.local en devel devel_php devel_generate easy_install php pacifica_uploader pacifica_synch
 drush @docker.local uli
