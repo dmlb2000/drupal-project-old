27006729613aafaa186dd16ac5b0fb9dd17ad224
David Brown <dmlb2000@gmail.com>
2019-07-27 21:15:24 -0700

    only composer install if we need to
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -xe
 
+RUN_COMPOSER=${RUN_COMPOSER:-$1}
 PROFILE=${PROFILE:-standard}
 export PATH="$PWD/vendor/bin:$PATH"
-composer install
-composer update
+if [[ ${RUN_COMPOSER} ]] ; then
+  composer install
+  composer update
+fi
 drush -y rsync . @docker.local
 drush -y @docker.local si --db-url=mysql://drupal:drupal@drupaldb:3306/drupal --site-name="$PROFILE site" $PROFILE
 drush -y @docker.local en devel devel_php devel_generate easy_install php pacifica_uploader pacifica_synch config_inspector
