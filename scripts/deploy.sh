7e635d622eadb5d9c09ec3a1695e31ad0eb6ecc7
David Brown <dmlb2000@gmail.com>
2019-08-03 13:55:54 -0700

    Add some migrate modules to run pacifica migrations
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,14 +12,14 @@ git checkout ${PACIFICA_VERSION}
 DOCKER_COMPOSE_CMD="docker-compose -f docker-compose.yml -f ../pacifica-docker-compose.override.yml"
 $DOCKER_COMPOSE_CMD pull
 $DOCKER_COMPOSE_CMD build --pull
-$DOCKER_COMPOSE_CMD up -d
+$DOCKER_COMPOSE_CMD up -d metadataserver mgmt policyserver
 popd
 pushd scripts/local-docker
 docker-compose build --pull
 docker-compose pull
 docker-compose up -d
-sleep 10
 popd
+sleep 10
 pushd scripts/pacifica
 MAX_TRIES=60
 HTTP_CODE=$(curl -sL -w "%{http_code}\\n" localhost:8121/keys -o /dev/null || true)
@@ -29,4 +29,6 @@ while [[ $HTTP_CODE != 200 && $MAX_TRIES > 0 ]] ; do
   MAX_TRIES=$(( MAX_TRIES - 1 ))
 done
 $DOCKER_COMPOSE_CMD exec metadataserver python /usr/src/app/tests/test_files/loadit_test.py
+$DOCKER_COMPOSE_CMD exec policyserver python -m pip install 'pacifica-metadata==0.10.3'
+$DOCKER_COMPOSE_CMD exec policyserver python /usr/src/app/tests/test_files/loadit_test.py
 popd
--- scripts/install.sh
+++ scripts/install.sh
@@ -7,10 +7,13 @@ if [[ ${RUN_COMPOSER} ]] ; then
   composer install
   composer update
 fi
+drush -y @docker.local ssh -- rm -rf docroot/modules/contrib
 drush -y rsync . @docker.local
 drush -y @docker.local si --db-url=mysql://drupal:drupal@drupaldb:3306/drupal --site-name="$PROFILE site" $PROFILE
-drush -y @docker.local en devel devel_php devel_generate easy_install php pacifica_uploader pacifica_synch config_inspector
-drush -y @docker.local cset pacifica_synch.settings pacifica_synch.enabled true
-drush -y @docker.local cset pacifica_synch.settings pacifica_synch.metadata.host metadataserver
-drush -y @docker.local cset pacifica_synch.settings pacifica_synch.amqp.host drupalamqp
+drush -y @docker.local en devel devel_php devel_generate easy_install php pacifica_uploader pacifica_synch config_inspector pacifica_migrate migrate_tools
+drush -y @docker.local cset pacifica_migrate.settings pacifica_migrate.database_settings.host metadatadb
+#drush -y @docker.local cset pacifica_synch.settings pacifica_synch.enabled true
+#drush -y @docker.local cset pacifica_synch.settings pacifica_synch.metadata.host metadataserver
+#drush -y @docker.local cset pacifica_synch.settings pacifica_synch.amqp.host drupalamqp
+drush @docker.local migrate:import --tag Pacifica
 drush @docker.local uli
